58ae1b3878eaea98a2839613b47878d20301c76e
Harsh Vatsal <153799575+harshvatsal41@users.noreply.github.com>
2024-10-24 16:22:33 +0530

    Done
    Done

--- src/App/store.js
+++ src/App/store.js
@@ -1,5 +1,8 @@
 import { configureStore } from '@reduxjs/toolkit';
-import productCartReducer from '../features/productCartSlice';
+import productCartReducer from '../features/productCartSlice.js';
+import { clearCart,removeFromCart,addToCart,decreaseAmount,increaseAmount } from "../Features/productCartSlice.js"
+
+
 
 export const store = configureStore({
     reducer: {
